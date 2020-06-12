33054ad160c922e7b20b25affcea3d2ebe4dadce
zhuluxi <luxi.zhu@alphayee.com>
2020-06-12 14:33:47 +0800

    first commit

---
+++ BackTracking/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BackTracking
+//
+//  Created by NewBoy on 2020/6/12.
+//  Copyright © 2020 newboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BackTracking/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  BackTracking
+//
+//  Created by NewBoy on 2020/6/12.
+//  Copyright © 2020 newboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
